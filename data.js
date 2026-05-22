322189837a42663460eb90537fc179b1c15ff49b
czsniper456 <filipek2152012@gmail.com>
2026-05-22 16:16:51 +0200

    Add tierPoints and players data structures

--- data.js
+++ data.js
@@ -1,1 +1,54 @@
+const tierPoints = {
+    "HT1": 10,
+    "LT1": 9,
+    "HT2": 8,
+    "LT2": 7,
+    "HT3": 6,
+    "LT3": 5,
+    "HT4": 4,
+    "LT4": 3,
+    "HT5": 2,
+    "LT5": 1,
 
+    "RHT1": 10,
+    "RLT1": 9,
+    "RHT2": 8,
+    "RLT2": 7,
+    "RHT3": 6,
+    "RLT3": 5,
+    "RHT4": 4,
+    "RLT4": 3,
+    "RHT5": 2,
+    "RLT5": 1
+};
+
+const players = [
+    {
+        name: "ExamplePlayer",
+        rankings: {
+            sword: "HT1",
+            axe: "LT2",
+            mace: "HT3",
+            uhc: "LT1",
+            nethpot: "HT2",
+            pot: "HT4",
+            smp: "LT3",
+            crystal: "HT1",
+            spearmace: "HT2"
+        }
+    },
+    {
+        name: "RetiredLegend",
+        rankings: {
+            sword: "RHT1",
+            axe: "RHT2",
+            mace: "RLT1",
+            uhc: "RHT3",
+            nethpot: "RLT2",
+            pot: "RHT4",
+            smp: "RLT3",
+            crystal: "RHT2",
+            spearmace: "RLT1"
+        }
+    }
+];
