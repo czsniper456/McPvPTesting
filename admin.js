766abca1ed2d07b17b66d523e4129cfbebe57ada
czsniper456 <filipek2152012@gmail.com>
2026-05-22 16:18:35 +0200

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,33 @@
+function login() {
+    const username = document.getElementById("githubUser").value;
 
+    if (username === "czsniper456") {
+        document.getElementById("loginBox").style.display = "none";
+        document.getElementById("adminPanel").style.display = "block";
+    } else {
+        alert("Access Denied");
+    }
+}
+
+function addPlayer() {
+    const playerName = document.getElementById("playerName").value;
+    const gamemode = document.getElementById("gamemode").value;
+    const tier = document.getElementById("tier").value;
+
+    let player = players.find(p => p.name === playerName);
+
+    if (!player) {
+        player = {
+            name: playerName,
+            rankings: {}
+        };
+
+        players.push(player);
+    }
+
+    player.rankings[gamemode] = tier;
+
+    document.getElementById("status").innerHTML = `Saved ${playerName} (${gamemode} = ${tier})`;
+
+    console.log(players);
+}
