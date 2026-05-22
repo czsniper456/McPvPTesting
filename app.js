6af85fde1ff5b4f861760ec5a083afe985e77074
czsniper456 <filipek2152012@gmail.com>
2026-05-22 18:54:50 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const tierPoints = {
     HT4: 4, LT4: 3, HT5: 2, LT5: 1,
     RHT1: 10, RLT1: 9, RHT2: 8, RLT2: 7, RHT3: 6, RLT3: 5,
     RHT4: 4, RLT4: 3, RHT5: 2, RLT5: 1
+}; // ← THIS WAS MISSING
+
 // Get the main content container
 const content = document.getElementById("content");
 
