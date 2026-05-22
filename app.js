4634ffdbdf7e02bf9ae15999d1c4b6d031876c8a
czsniper456 <filipek2152012@gmail.com>
2026-05-22 17:05:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,4 @@
 const content = document.getElementById("content");
-
-const tierPoints = {
-    HT1: 10,
-    LT1: 9,
-    HT2: 8,
-    LT2: 7,
-    HT3: 6,
-    LT3: 5,
-    HT4: 4,
-    LT4: 3,
-    HT5: 2,
-    LT5: 1,
-
-    RHT1: 10,
-    RLT1: 9,
-    RHT2: 8,
-    RLT2: 7,
-    RHT3: 6,
-    RLT3: 5,
-    RHT4: 4,
-    RLT4: 3,
-    RHT5: 2,
-    RLT5: 1
-};
-
 function getPlayers() {
     return JSON.parse(localStorage.getItem("mcpvp_players")) || [];
 }
